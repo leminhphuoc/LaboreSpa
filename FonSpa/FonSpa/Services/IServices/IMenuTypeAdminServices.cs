f41ff13c0d9f700f9e8d27627b9c89553d5bac26
kevinle021097 <32714899+kevinle021097@users.noreply.github.com>
2019-11-10 10:51:17 +0700

    10-11

---
+++ FonSpa/FonSpa/Services/IServices/IMenuTypeAdminServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FonSpa.Services.IServices
+{
+    public class IMenuTypeAdminServices
+    {
+    }
+}
---
+++ FonSpa/FonSpa/Services/IServices/IServiceCategoryAdminServices.cs
@@ -0,0 +1,19 @@
+﻿using Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FonSpa.Services.IServices
+{
+    public interface IServiceCategoryAdminServices
+    {
+        List<ServiceCategory> ListAllByName(string searchString);
+        List<ServiceCategory> GetServiceCategory();
+        long AddServiceCategory(ServiceCategory servicecategory);
+        bool Delete(int id);
+        ServiceCategory GetDetail(int id);
+        bool Edit(ServiceCategory servicecategory);
+        bool? ChangeStatus(int id);
+    }
+}
